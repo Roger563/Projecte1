d118a5928ac222b9d9b9c680c8e815fe743196fa
Roger <Aguilar>
2021-05-09 19:03:11 +0200

    Main menu and pause menu (without art)

---
+++ Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class InGameMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool Paused;
+    public GameObject panel;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (Paused)
+            {
+                Resume();
+            }
+            else if (!Paused)
+            {
+                PauseMenu();
+            }
+            
+        }
+
+        
+    }
+    void Resume()
+    {
+        panel.SetActive(false);
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+
+    void PauseMenu()
+    {
+        panel.SetActive(true);
+        Time.timeScale = 0f;
+        Paused = true;
+    }
+    public void MenuButton()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+   public void ResetButton()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Paused = false;
+       
+    }
+}
