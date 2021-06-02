433a49ca6419b66c635faadebad8f532ac17ee4f
Pol <pol.valverde@enti.cat>
2021-06-02 20:05:56 +0200

    Menú entre nivells

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using TMPro;
 public class NextLevel : MonoBehaviour
 {
+    public GameObject UI;
+    public TMP_Text timer;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+           
+            Time.timeScale = 0;
+            UI.transform.GetChild(UI.transform.childCount-1).gameObject.GetComponent<TMP_Text>().text = timer.text;
+            UI.SetActive(true);
+            // UI.GetComponentInChildren<TMP_Text>().text=timer.text;
+           
         }
     }
 }
