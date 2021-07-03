f36449baec7af75f255aa08a5b210f83eb92f7e5
Eric <eric.ruiz@enti.cat>
2021-07-03 11:54:48 +0200

    Level selector menu

---
+++ Assets/Scripts/LevelSelectButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectButton : MonoBehaviour
+{
+    public int Level;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SkipLevel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipLevel : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+            SceneManager.LoadScene(1);
+
+        if (Input.GetKeyDown("2"))
+            SceneManager.LoadScene(2);
+
+        if (Input.GetKeyDown("3"))
+            SceneManager.LoadScene(3);
+
+        if (Input.GetKeyDown("4"))
+            SceneManager.LoadScene(4);
+
+        if (Input.GetKeyDown("5"))
+            SceneManager.LoadScene(5);
+
+        if (Input.GetKeyDown("6"))
+            SceneManager.LoadScene(6);
+
+        if (Input.GetKeyDown("7"))
+            SceneManager.LoadScene(7);
+
+        if (Input.GetKeyDown("8"))
+            SceneManager.LoadScene(8);
+
+        if (Input.GetKeyDown("9"))
+            SceneManager.LoadScene(9);
+    }
+}
